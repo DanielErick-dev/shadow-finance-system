75735b34b97089cf8c3103be97aedf6f1966d8a1
Daniel Erick <DanielErick.estudante@gmail.com>
2025-09-19 15:48:05 -0300

    feature: add button 'Entrar Como Visitante'

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -9,6 +9,17 @@ export default function Login() {
     const [isSubmitting, setIsSubmitting] = useState(false);
     const { login } = useAuth();
     
+    const handleDemonstrativeUser = async (event: React.FormEvent) => {
+        event.preventDefault();
+        setError(null)
+        setIsSubmitting(true)
+        try{
+            await login('teste', 'test@123');
+        } catch(error){
+            setError('acesso negado! Credencias inválidas.');
+            setIsSubmitting(false)
+        }
+    };
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
         setError(null);
@@ -103,6 +114,22 @@ export default function Login() {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
                         </svg>
                         { isSubmitting ? 'CONECTANDO...': 'ACESSAR SISTEMA'}
+                    </button>
+                    <button
+                        type="button"
+                        onClick={handleDemonstrativeUser}
+                        className="w-full bg-gradient-to-r from-gray-700 to-gray-600 hover:from-gray-600 hover:to-gray-500 text-white font-bold py-3 px-4 rounded-lg shadow-lg hover:shadow-gray-500/20 transition-all duration-300 flex items-center justify-center mt-3"
+                        disabled={isSubmitting}
+                    >
+                        <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z"
+                        />
+                        </svg>
+                        ENTRAR COMO VISITANTE
                     </button>
                 </form>
                 <div className="bg-black/50 p-4 border-t border-purple-500/10 text-center">
