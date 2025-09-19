fda74214c2d62ebe62b92d332beb5563c0d7c77f
Daniel Erick <DanielErick.estudante@gmail.com>
2025-09-19 15:47:27 -0300

    refactor: add DJANGO_ENV to define production ou development ambient

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -7,6 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
+DJANGO_ENV = config('DJANGO_ENV', default='development')
 
 ALLOWED_HOSTS = []
 if DEBUG:
@@ -62,11 +63,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "core.wsgi.application"
 
-DATABASES = {
-    "default": dj_database_url.config(
-        conn_max_age=600
-    )
-}
+if DJANGO_ENV == 'PRODUCTION':
+    DATABASES = {
+        "default": dj_database_url.config(
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
